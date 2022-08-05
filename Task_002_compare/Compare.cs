cd4dcb99593717de8ff9ba78c874446fdcd3c3ca
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-05 21:41:37 +0300

    Task 002 - is it Squer of each other?

---
+++ Task_002_compare/Compare.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите пожалуйста первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите пожалуйста второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else if (number2 == number1 * number1)
+{
+    Console.WriteLine("Второе число является квадратом первого числа");
+}
+else 
+{
+    Console.WriteLine("Ни первое, ни Второе число не являются квадратом.");
+}
