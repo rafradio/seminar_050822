a0b7a14ff9ff0489d2235068634fbe91d72d946f
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-05 21:28:45 +0300

    Some changes for output in WriteLine

--- Task_001_squre/Program.cs
+++ Task_001_squre/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 Console.Write("Введите пожалуйста число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int output = Convert.ToInt32(Math.Pow(number, 2));
+//int output = Convert.ToInt32(Math.Pow(number, 2));
 //int output = number * number;
-Console.WriteLine("Квадрат данного числа равен = " + output);
+Console.WriteLine("Квадрат данного числа равен = " + Convert.ToInt32(Math.Pow(number, 2)));
