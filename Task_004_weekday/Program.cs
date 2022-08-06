5b5036a80cc093fb29b2601d11383e93833a117e
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-06 12:24:39 +0300

    Finished with weekday

--- Task_004_weekday/Program.cs
+++ Task_004_weekday/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Clear();
+string[] weekDays = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
 Console.Write("Введите пожалуйста число (от 1 до 7): ");
 string number1 = Console.ReadLine();
 int number2;
@@ -7,14 +8,14 @@ if (int.TryParse(number1, out int output))
     number2 = Convert.ToInt32(number1);
     if (number2 > 0 && number2 < 8)
     {
-        Console.WriteLine("Это число " + number2);
+        Console.WriteLine("Сегодня - " + weekDays[number2 - 1]);
     }
     else
     {
-        Console.WriteLine("Вы ввели неправильное число - " + number2);
+        Console.WriteLine("Вы ввели некорректное число - " + number2);
     }
 }
 else
 {
-    Console.WriteLine("Это не число - " + number1);
+    Console.WriteLine("Вы ввели не число - " + number1);
 }
