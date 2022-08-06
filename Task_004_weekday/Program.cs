0e32e35e392660b107a8b5434cfc9f5d20486717
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-06 12:13:58 +0300

    Starting work with weekday

---
+++ Task_004_weekday/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите пожалуйста число (от 1 до 7): ");
+string number1 = Console.ReadLine();
+int number2;
+if (int.TryParse(number1, out int output))
+{
+    number2 = Convert.ToInt32(number1);
+    if (number2 > 0 && number2 < 8)
+    {
+        Console.WriteLine("Это число " + number2);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели неправильное число - " + number2);
+    }
+}
+else
+{
+    Console.WriteLine("Это не число - " + number1);
+}
