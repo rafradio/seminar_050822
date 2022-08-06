10a9a5d9a6ef097d4dcf0dd5151694213b309d02
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-06 15:08:42 +0300

    homework for 050822 - is it even number inputed by user

---
+++ HW050822_isitEven/NumberEven.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите пожалуйста число: ");
+string number1 = Console.ReadLine();
+if (int.TryParse(number1, out int number))
+{
+    if ((number % 2) == 0)
+    {
+        Console.WriteLine("Данное число ЧЕТНОЕ: " + number1);
+    }
+    else 
+    {
+        Console.WriteLine("Данное число НЕЧЕТНОЕ: " + number1);
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели либо строчку, либо дробное число");
+}
