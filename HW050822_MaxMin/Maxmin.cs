aadfec5de0d2bee1e3dee32be1b2c697997446df
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-07 13:16:11 +0300

    Homework Max-Min: modifing for Class section

--- HW050822_MaxMin/Maxmin.cs
+++ HW050822_MaxMin/Maxmin.cs
@@ -1,12 +1,24 @@
-﻿int number1 = 5;
-int number2 = 10;
-if (number1 > number2)
+﻿using System;
+
+namespace MaxMin
 {
-    int max = number1;
-    Console.WriteLine("Первое число больше -" + max + ", а второе число меньше - " + number2);
-}
-else
-{
-    int max = number2;
-    Console.WriteLine("Второе число больше -" + max + ", а первое число меньше - " + number1);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 90;
+            int number2 = 10;
+            if (number1 > number2)
+            {
+                int max = number1;
+                Console.WriteLine("Первое число больше: " + max + ", а второе число меньше: " + number2);
+            }
+            else
+            {
+                int max = number2;
+                Console.WriteLine("Второе число больше: " + max + ", а первое число меньше: " + number1);
+            }
+        }
+    }
+
 }
