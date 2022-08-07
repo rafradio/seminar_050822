c70d285ed35618a53d3362ccc012ed69688b902d
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-07 13:04:48 +0300

    Homework Max-Min beetwin two numbers: starting poin

---
+++ HW050822_MaxMin/Maxmin.cs
@@ -0,0 +1,12 @@
+﻿int number1 = 5;
+int number2 = 10;
+if (number1 > number2)
+{
+    int max = number1;
+    Console.WriteLine("Первое число больше -" + max + ", а второе число меньше - " + number2);
+}
+else
+{
+    int max = number2;
+    Console.WriteLine("Второе число больше -" + max + ", а первое число меньше - " + number1);
+}
